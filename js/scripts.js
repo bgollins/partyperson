28afd438501195847d2f284b7400402124195d4d
Brittany Gollins <brittany.gollins@gmail.com>
2018-09-18 16:27:39 -0700

    resolve syntax error: missing quotes around id as variable. new issue- trying to add if/else conditions but running into errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
-  $("form").submit(function(event) {
-    var dishwasher = parseInt($(#dishwasher)).val());
-    var food = parseInt($(#food)).val());
-    var outfit = parseInt($(#outfit)).val());
-    var drink = parseInt($(#drink)).val());
-    var space = parseInt($(#space)).val());
+  $("#questions form").submit(function(event) {
+    var dishwasher = parseInt($("#dishwasher").val());
+    var food = parseInt($("#food").val());
+    var outfit = parseInt($("#outfit").val());
+    var drink = parseInt($("#drink").val());
+    var space = parseInt($("#space").val());
     var total = dishwasher + food + outfit + drink + space;
 
-  event.preventDefault();
+    if (total <16) {
+      $('#netflix').show();
+    } else if (total >=16 && <31){
+      $('#potluck').show();
+    } else if (total >=30 && <66){
+      $('#bbq').show();
+    } else if (total >=65){
+      $('#cocktail').show();
+    }
+
+    event.preventDefault();
   });
 });
