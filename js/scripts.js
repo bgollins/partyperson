7b0079ef9dd5e05bae46e4acfc8a4ae1c7d1de69
Brittany Gollins <brittany.gollins@gmail.com>
2018-09-18 12:30:57 -0700

    add lines for JavaScript to get the value from the simple select inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-  
+  $("form").submit(function(event) {
+    var dishwasher = parseInt($(#dishwasher)).val());
+    var food = parseInt($(#food)).val());
+    var outfit = parseInt($(#outfit)).val());
+    var drink = parseInt($(#drink)).val());
+    var space = parseInt($(#space)).val());
+    var total = dishwasher + food + outfit + drink + space;
+
+  event.preventDefault();
+  });
 });
